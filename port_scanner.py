2f551231f674fc02a4dfcb5aa02c7787eec923a3
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-13 12:49:36 +0530

    Optimized timeout to 0.05s, and used with statement

--- port_scanner.py
+++ port_scanner.py
@@ -3,11 +3,10 @@ ip="127.0.0.1"
 print(f"Scanning {ip} .......")
 print("-"*30)
 for port in range(1,1025):
-    sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(0.5)
-    result= sock.connect_ex((ip, port))
-    if result==0:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+     sock.settimeout(0.05)
+     result= sock.connect_ex((ip, port))
+     if result==0:
         print(f"Port {port} is OPEN")
-    sock.close()
 print("-"*30)
 print("Scan Complete")
