26476d5d53af2c227b54b7043e74e1b83f0b8e2e
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-13 11:35:56 +0530

    Added loop to scan ports 1-1024

--- port_scanner.py
+++ port_scanner.py
@@ -1,10 +1,12 @@
 import socket
 ip="127.0.0.1"
-port=135
-sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result= sock.connect_ex((ip, port))
-if result==0:
-    print(f"Port {port} is OPEN")
-else:
-    print(f"Port {port} is CLOSED(error code: {result})")
-sock.close()
+print(f"Scanning {ip} .......")
+print("-"*30)
+for port in range(1,1025):
+    sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result= sock.connect_ex((ip, port))
+    if result==0:
+        print(f"Port {port} is OPEN")
+    sock.close()
+print("-"*30)
+print("Scan Complete")
