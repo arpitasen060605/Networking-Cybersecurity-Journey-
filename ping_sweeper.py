72c03d99bb2faacfbb60ecc4a113a89afe4ec15a
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-19 17:00:11 +0530

    Added threading for fast parallel ping sweep

--- ping_sweeper.py
+++ ping_sweeper.py
@@ -1,18 +1,23 @@
 import subprocess 
+import threading 
 network= "192.168.29."
-print(f"Sweeping(Scanning) {network}0/24...")
-print("-"*30)
-
 alive_hosts= []
-
-for i in range(1,255):   
-     ip= network + str(i)
-     result= subprocess.run(["ping", "-n","1", "-w", "300", ip], capture_output= True, text= True) 
-
-     if result.returncode==0:
-        print(f"{ip} is Active")
-        alive_hosts.append(ip)
-
+lock= threading.Lock()
+def ping_ip(ip):
+    result= subprocess.run(["ping","-n","1","-w","300",ip],capture_output= True, text=True)
+    if result.returncode==0:
+        with lock:
+            alive_hosts.append(ip)
+            print(f"{ip} is Active")
+print(f"Sweeping the network {network}0/24....")
 print("-"*30)
-print(f"Sweep(Scan)Complete, {len(alive_hosts)} hosts are active.")
-
+threads=[]
+for i in range(1,255):
+    ip= network + str(i)
+    t= threading.Thread(target=ping_ip, args=(ip,))
+    threads.append(t)
+    t.start()
+for t in threads:
+    t.join()
+print("-"*30)
+print(f"Sweep/Scan Complete {len(alive_hosts)} hosts are active")
