9b02a901d4a221e4f7a14e700da15c10e5c2f10b
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-19 16:37:16 +0530

    Added ping sweep across full subnet range

--- ping_sweeper.py
+++ ping_sweeper.py
@@ -1,7 +1,18 @@
 import subprocess 
-ip= "127.0.0.1"
-result= subprocess.run(["ping", "-n","1", ip], capture_output= True, text= True)
-if result.returncode==0:
-    print(f"{ip} is Active")
-else:
-    print(f"{ip} is Dead")
+network= "192.168.29."
+print(f"Sweeping(Scanning) {network}0/24...")
+print("-"*30)
+
+alive_hosts= []
+
+for i in range(1,255):   
+     ip= network + str(i)
+     result= subprocess.run(["ping", "-n","1", "-w", "300", ip], capture_output= True, text= True) 
+
+     if result.returncode==0:
+        print(f"{ip} is Active")
+        alive_hosts.append(ip)
+
+print("-"*30)
+print(f"Sweep(Scan)Complete, {len(alive_hosts)} hosts are active.")
+
