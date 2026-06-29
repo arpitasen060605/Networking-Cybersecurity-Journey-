40dc2b6a29b0f1a30a2286aa3a0b37548471d124
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-29 14:51:59 +0530

    Added local IP and gateway Detection

---
+++ network_dashboard.py
@@ -0,0 +1,23 @@
+import socket
+import subprocess
+import re 
+def get_local_ip():
+     s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     try:
+        s.connect(("8.8.8.8", 80))
+        ip= s.getsockname()[0]
+     except Exception:
+        ip= "127.0.0.1"
+     finally:
+        s.close()
+     return ip
+print("Local IP-", get_local_ip())
+
+def get_gateway():
+   result= subprocess.run(["ipconfig"], capture_output= True, text=True)
+   output= result.stdout
+   match= re.search(r"Default Gateway[. ]*:[^\n]*\n?\s*([\d.]+)",output)
+   if match:
+      return match.group(1)
+   return "Not found"
+print("Gateway-", get_gateway())
