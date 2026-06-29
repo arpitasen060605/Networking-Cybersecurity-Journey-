d5e0953beff75bc29cfaebd864666bdf9414975c
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-29 15:42:12 +0530

    Organized Network Information into clean report

--- network_dashboard.py
+++ network_dashboard.py
@@ -1,6 +1,7 @@
 import socket
 import subprocess
 import re 
+
 def get_local_ip():
      s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
      try:
@@ -11,22 +12,22 @@ def get_local_ip():
      finally:
         s.close()
      return ip
-print("Local IP-", get_local_ip())
 
 def get_gateway():
    result= subprocess.run(["ipconfig"], capture_output= True, text=True)
    output= result.stdout
    match= re.search(r"Default Gateway[. ]*:[^\n]*\n?\s*([\d.]+)",output)
-   if match:
-      return match.group(1)
-   return "Not found"
-print("Gateway-", get_gateway())
-
+   return match.group(1) if match else "Not Found"
+      
 def get_dns():
    result = subprocess.run(["ipconfig","/all"], capture_output= True, text= True)
    output= result.stdout
    match= re.search(r"DNS Servers[. ]*:[^\n]*\n?\s*([\d.]+)", output)
-   if match:
-      return match.group(1)
-   return "Not Found"
-print("DNS-", get_dns())
+   return match.group(1) if match else "Not Found"
+
+print("\n Network Information")
+print(" "+ "-"*30)
+print(f"Local IP- {get_local_ip()}")
+print(f"Gateway- {get_gateway()}")
+print(f"DNS- {get_dns()}")
+print(" "+ "-"*30 + "\n")
