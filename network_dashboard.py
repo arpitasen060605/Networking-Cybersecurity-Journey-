a799953e390948c91ff9e4e01100ecd1e960f688
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-29 15:29:28 +0530

    Added DNS server detection

--- network_dashboard.py
+++ network_dashboard.py
@@ -21,3 +21,12 @@ def get_gateway():
       return match.group(1)
    return "Not found"
 print("Gateway-", get_gateway())
+
+def get_dns():
+   result = subprocess.run(["ipconfig","/all"], capture_output= True, text= True)
+   output= result.stdout
+   match= re.search(r"DNS Servers[. ]*:[^\n]*\n?\s*([\d.]+)", output)
+   if match:
+      return match.group(1)
+   return "Not Found"
+print("DNS-", get_dns())
