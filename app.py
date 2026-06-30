9dc2d7a095a079aee0e19827cc9a28954d96bdb0
ARPITA SEN <senarpita06062005@gmail.com>
2026-06-30 15:14:13 +0530

    Added latency/packet loss and auto-refresh

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, render_template, request 
-from network_dashboard import (get_local_ip, get_gateway, get_dns, get_cpu_usage, get_ram_usage, get_network_prefix, get_connected_devices, get_open_ports)
+from network_dashboard import (get_local_ip, get_gateway, get_dns, get_cpu_usage, get_ram_usage, get_network_prefix, get_connected_devices, get_open_ports, get_latency_and_loss)
 app= Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,8 +13,11 @@ def home():
     network_prefix= get_network_prefix(local_ip)
     devices= get_connected_devices(network_prefix)
 
+    avg_latency, packet_loss = get_latency_and_loss()
+
     open_ports= None
     selected_device= None
+
     if request.method== "POST":
        selected_device= request.form.get("device")
        if selected_device:
@@ -28,7 +31,9 @@ def home():
      ram=ram,
     devices= devices,
     open_ports=open_ports,
-    selected_device= selected_device
+    selected_device= selected_device,
+    avg_latency= avg_latency,
+    packet_loss= packet_loss
     )
 if __name__=="__main__":
     app.run(debug=True)
